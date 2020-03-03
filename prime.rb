87027431ac35f79661868eceef19209bd5c00ba6
Josh Ellis <joshe523@gmail.com>
2020-03-03 04:01:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@
 def prime?(n)
   return false if n < 1
   return true if n == 2
-  (1..n-1).each { |int| return false if n % int == 0 }
+  (1..n-1).to_a.each { |int| return false if n % int == 0 }
   return true
 end
